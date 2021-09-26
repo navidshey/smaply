57918e39ae7e9faa88e09c1a485289703b255169
navid <sheydaein@gmail.com>
2021-09-26 17:23:37 +0330

    sth

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import { ThemeProvider, createTheme } from "@mui/material/styles";
 import React, { Suspense  } from "react";
 import { Provider } from "react-redux";
 import Inter from "./assets/font/Inter/Inter-Regular.ttf";
-import Spinner from "./components/custom/spinner/Spinner";
 import MyRoute from "./components/custom/other/MyRoute";
 import store from "./store/store";
 import { ErrorBoundary } from "react-error-boundary";
 import ErrorFallback from "./components/custom/other/ErrorFallback";
 import { SnackbarContainer } from './context/SnackbakContext';
 import CustomSnackBar from './components/custom/snackBar/CustomSnackbar';
+import Spinner from './components/custom/spinner/Spinner';
 
 
 const theme = createTheme({
